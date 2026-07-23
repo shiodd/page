e821188e9e8dc18392c2a4bc46646bf932ec746b
shiodd <2523195235@qq.com>
2026-07-23 16:46:32 +0800

    update

--- html/data/friends.js
+++ html/data/friends.js
@@ -1,6 +1,5 @@
 // 友链数据在 HTML 外维护：data/friends.js（定义全局 friendsData）
 const friendsData = [
-        { name: "测试", front: "../image/friends/friend-1-front.png", back: "../image/friends/friend-1-back.png" },
-        { name: "ShionP", front: "../image/friends/friend-2-front.png", back: "../image/friends/friend-2-back.png", url: "https://shiodd.github.io/page" },
+        { name: "ShionP(测试用)", front: "../image/friends/friend-2-front.png", back: "../image/friends/friend-2-back.png", url: "https://shiodd.github.io/page" },
     // NEW_FRIEND
 ];
